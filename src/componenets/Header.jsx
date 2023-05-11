e9921d9a041cfb5cbffe8c9e69a65a96f4edcd73
Kamakshee Samant <samant.kamakshee@gmail.com>
2023-05-11 12:36:31 +1000

    chore: add header menu responsive

--- src/componenets/Header.jsx
+++ src/componenets/Header.jsx
@@ -1,42 +1,59 @@
-import React from "react";
+import React, { useRef } from "react";
 import { GrRestaurant } from "react-icons/gr";
 import { BsFillSearchHeartFill } from "react-icons/bs";
-import { AiOutlineMenu } from "react-icons/ai";
+import { AiOutlineMenu, AiOutlineShoppingCart } from "react-icons/ai";
 import "./header.scss";
 import { Container } from "reactstrap";
 
 import { navLinks } from "../utils/constants";
 
 const Header = () => {
+  const menuRef = useRef();
+  const menuToggle = () => menuRef.current.classList.toggle("active__menu");
+
   return (
     <header className="header">
       <Container>
         <div className="navigation">
           <div className="logo">
-            <h2 className="d-flex align-items-center gap-1">
-              <span>
-                <GrRestaurant /> Master Chef Food
-              </span>
+            <h2 className=" d-flex align-items-center gap-1">
+              <GrRestaurant />
+              Master Chef Food
             </h2>
           </div>
 
-          <div className="nav__menu">
-            <ul className="nav__list">
-              {navLinks.map((item, i) => (
-                <li className="nav__item" key={`${i}nth_navigation`}>
-                  <a href={item.url}>{item.name}</a>
-                </li>
-              ))}
-            </ul>
-          </div>
-          <div className="menu__right">
-            <div className="custom__search ">
-              <input type="" placeholder="Saerch items here..."></input>
-              <BsFillSearchHeartFill />
+          <div className="nav__menu " ref={menuRef}>
+            <div className="nav__list__wrapper d-flex align-items-center gap-5">
+              <ul className="nav__list">
+                {navLinks.map((item, index) => (
+                  <li className="nav__item" key={index}>
+                    <a href={item.url} onClick={menuToggle}>
+                      {item.name}
+                    </a>
+                  </li>
+                ))}
+              </ul>
+
+              <div className="menu__right">
+                <div className="custom__search ">
+                  <input type="text" placeholder="search item...." />
+                  <BsFillSearchHeartFill />
+                </div>
+              </div>
             </div>
           </div>
+
+          <div>
+            <span className="cart__icon">
+              <AiOutlineShoppingCart />
+              <span className="badge">2</span>
+            </span>
+          </div>
+
           <div className="mobile__menu">
-            <AiOutlineMenu />
+            <span>
+              <AiOutlineMenu onClick={menuToggle} />
+            </span>
           </div>
         </div>
       </Container>
