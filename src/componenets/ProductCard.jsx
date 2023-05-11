726b6dd12a63402ad6a9a107dd59f20fea6238da
Kamakshee Samant <samant.kamakshee@gmail.com>
2023-05-11 14:21:57 +1000

    chore: add popular food menu

--- src/componenets/ProductCard.jsx
+++ src/componenets/ProductCard.jsx
@@ -1,7 +1,42 @@
 import React from "react";
+import { AiFillStar, AiOutlineShoppingCart } from "react-icons/ai";
+import "./productCard.scss";
 
-const ProductCard = () => {
-  return <div>ProductCard</div>;
+const ProductCard = ({ item }) => {
+  return (
+    <div className="single__product">
+      <div className="product__img">
+        <img src={item.imgUrl} alt="" className="w-100" />
+      </div>
+      <div className="product__content">
+        <div className="rating text-center">
+          <span>
+            <AiFillStar className="ri-star-s-fill" />
+          </span>
+          <span>
+            <AiFillStar className="ri-star-s-fill" />
+          </span>
+          <span>
+            <AiFillStar className="ri-star-s-fill" />
+          </span>
+          <span>
+            <AiFillStar className="ri-star-s-fill" />
+          </span>
+          <span>
+            <AiFillStar className="ri-star-s-fill" />
+          </span>
+        </div>
+        <h6>{item.title}</h6>
+        <div className="d-flex align-items-center justify-content-between">
+          <span className="price">
+            {" "}
+            Price: $<span>{item.price}</span>
+          </span>
+          <AiOutlineShoppingCart />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ProductCard;
--- src/componenets/index.js
+++ src/componenets/index.js
@@ -1,3 +1,4 @@
 export { default as Header } from "./Header";
 export { default as Slider } from "./Slider";
 export { default as PopularMenu } from "./PopularMenu";
+export { default as ProductCard } from "./ProductCard";
